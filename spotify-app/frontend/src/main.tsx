2d9405e29166a3f6513516bc8a73c9f8518dcd7a
Manogna <manu@Manognas-MacBook-Air.local>
2025-04-22 12:08:38 +0530

    first commit

---
+++ spotify-app/frontend/src/main.tsx
@@ -0,0 +1,48 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.tsx";
+// import { ClerkProvider } from "@clerk/clerk-react";
+// import { BrowserRouter } from "react-router-dom";
+// import AuthProvider from "./providers/AuthProvider.tsx";
+
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+// if (!PUBLISHABLE_KEY) {
+// 	throw new Error("Missing Publishable Key");
+// }
+
+// createRoot(document.getElementById("root")!).render(
+// 	<StrictMode>
+// 		<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+// 			<AuthProvider>
+// 				<BrowserRouter>
+// 					<App />
+// 				</BrowserRouter>
+// 			</AuthProvider>
+// 		</ClerkProvider>
+// 	</StrictMode>
+// );
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+  </React.StrictMode>,
+)
